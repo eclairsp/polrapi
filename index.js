b42397757917109ab18c5731e1671dfbb62747a5
Prabhjyot Singh Dhillon <psd231998@gmail.com>
2020-08-11 22:51:03 +0530

    heroku deploy

---
+++ index.js
@@ -0,0 +1,31 @@
+import "dotenv/config";
+import "./src/db/db";
+
+import http from "http";
+import express from "express";
+import {ApolloServer} from "apollo-server-express";
+import schema from "./schema";
+import Query from "./src/resolvers/Query";
+import Mutation from "./src/resolvers/Mutation";
+import Subscription from "./src/resolvers/Subscription";
+
+const app = express();
+
+const resolvers = {
+    Query,
+    Mutation,
+    Subscription,
+};
+
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+});
+
+server.applyMiddleware({app, path: "/graphql"});
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+httpServer.listen({port: process.env.PORT}, () => {
+    console.log(`Apollo Server running`);
+});
